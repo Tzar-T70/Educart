aac75ac83c4e95e731583464414c7e1bcfbb1d85
Itsmike1 <240106397@aston.ac.uk>
2025-12-01 10:46:57 +0000

    Create BasketPage.blade.php

---
+++ resources/views/payments/BasketPage.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" >
+    <title>EDUCART - Bag</title>
+    <style>
+        h1 {
+            margin-top: 50px;
+            text-align: left;
+        }
+        h2 {
+            margin-top: 75px;
+            text-align: left;
+        }
+        h3 {
+            margin-right: 150px;
+            text-align: right;
+        }
+        p {
+            margin-right: 100px;
+            text-align: right;
+        }
+        table {
+            border-collapse: collapse;
+            margin-left: 0;
+        }
+
+       .search-container {
+    position: absolute;
+    top: 20px;   
+    right: 20px;
+}
+
+.search-bar {
+    padding: 6px 10px;
+    border-radius: 5px;
+    
+}
+      
+    </style>
+</head>
+<body>
+    <h1>EDUCART</h1>
+
+    <h2>Bag</h2>
+    <div class="search-container">
+    <input type="text">
+    <button>Search</button>
+</div>
+
+
+    <table>
+        <tr>
+            <th>Item</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            
+        </tr>
+    </table>
+
+    <h3><strong>Summary</strong></h3>
+
+    <p>
+       Do you have a promo code?<br>
+      
+
+    <label for="promo">Enter promo code:</label>
+    <input type="text" id="promo" placeholder="Type code here">
+    <br><br>
+
+    Subtotal:<br>
+    Estimated delivery<br><br>
+
+    <a href="Checkout.html">CHECKOUT</a>
+</p>
+
+       
+        
+
+       </a>
+    </p>
+</body>
+</html>
